dbce3f5bd7a5dacaac455e314cab9079ba52d60e
covacg <ca.cg@lasallistas.org.mx>
2023-09-18 21:04:38 -0600

    Apunte de clase en el que se desarrolló borrados y creaciones

--- sams/views.py
+++ sams/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Vendor,Product
+from .forms import CreateVendorForm, CreateProductsForm
 # Create your views here.
 def home (request):
+    #Hola
     return render(request,'home.html',{})
+
+def vendor(request):
+    vendor_list= Vendor.objects.all() #retornar una lista de vendedores
+    #F= se nombra primero el data_context después la variable creada
+    data_context={'vendor_list':vendor_list}
+    vendor_form=CreateVendorForm()
+    data_context['vendor_form']=vendor_form
+    if request.method =='POST':
+        vendor_form=CreateVendorForm(request.POST)
+        print(vendor_form.errors)
+        print(request.POST)
+        if vendor_form.is_valid():
+            try:
+                vendor_validate=Vendor.objects.get(vendor_name=request.POST.get('vendor_name'))
+                if vendor_validate is not None:
+                    data_context['vendor_exist']=True
+            except: # =catch
+                vendor_form.save()                  
+    return render(request,'vendor.html',data_context)
+
+def product(request):#filter te trae una lista de objetos que cumplen con una restricción
+    product_list= Product.objects.filter(deleted_date__isnull = True) #retornar una lista de vendedores
+    #F= se nombra primero el data_context después la variable creada
+    data_context={'product_list':product_list}
+    product_form=CreateProductsForm()
+    data_context['product_form']=product_form
+    if request.method =='POST':
+        product_form=CreateProductsForm(request.POST)
+        print(product_form.errors)
+        print(request.POST)
+        if product_form.is_valid():
+            try:
+                product_validate=Product.objects.get(product_sku=request.POST.get('product_sku'))
+                if product_validate is not None:
+                    data_context['product_exist']=True
+            except: # =catch
+                product_form.save()                  
+    return render(request,'product.html',data_context)
+
+def deleteproduct(request,product_id):
+    product = Product.objects.get(pk=product_id)
+    #1° nombre del valor, 2° Nombre de la variable
+    data_context = {'product':product}
+    if request.method =='POST':
+        print(request.POST)
+        if 'yes' in request.POST:
+            product.deleted_date=timezone.now()
+            product.save()
+            return redirect('home')
+    return render(request,'delete_product.html',data_context)
